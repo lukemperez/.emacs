c82790557a9265007833e225d989b774525cf45f
Luke M Perez <lukemperez@gmail.com>
2018-08-09 07:36:15 -0500

    reloaded solarized theme

--- .cache/customize.el
+++ .cache/customize.el
@@ -3,9 +3,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(nrepl-message-colors
+   (quote
+    ("#CC9393" "#DFAF8F" "#F0DFAF" "#7F9F7F" "#BFEBBF" "#93E0E3" "#94BFF3" "#DC8CC3")))
  '(package-selected-packages
    (quote
-    (atom-one-dark-theme atom-dark-theme zenburn-theme use-package pandoc-mode markdown-mode auctex))))
+    (github-theme solarized-theme atom-one-dark-theme atom-dark-theme zenburn-theme use-package pandoc-mode markdown-mode auctex)))
+ '(pdf-view-midnight-colors (quote ("#DCDCCC" . "#383838"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
