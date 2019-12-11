d1a386b684e9ac445a8748ced1c2127be1283504
Luke M. Perez <lukemperez@gmail.com>
2019-12-11 08:57:22 -0700

    linking to new MBP

--- .cache/customize.el
+++ .cache/customize.el
@@ -3,12 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-agenda-files
-   (quote
-    ("~/Dropbox/AcademicWork/Projects/Inprogress/libfaith_mss/lf_mss.org")))
  '(package-selected-packages
    (quote
-    (org-ref zenburn-theme use-package solarized-theme pandoc-mode markdown-mode auctex atom-one-dark-theme))))
+    (pandoc solarized-theme zenburn-theme pandoc-mode markdown-mode use-package org-ref auctex))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
