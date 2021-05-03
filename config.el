0d97c7e219092cfa4f1d539b1b3e1d67fad60929
Luke M. Perez <lukemperez@gmail.com>
2021-05-03 15:33:38 -0700

    updates

--- config.el
+++ config.el
@@ -28,6 +28,47 @@ Should end with a forward slash.")
 (setq ido-everywhere t)
 (ido-mode 1)
 
+(defun my-eshell-prompt ()
+  "Highlight eshell pwd and prompt separately."
+  (mapconcat
+   (lambda (list)
+     (propertize (car list)
+                 'read-only      t
+                 'font-lock-face (cdr list)
+                 'front-sticky   '(font-lock-face read-only)
+                 'rear-nonsticky '(font-lock-face read-only)))
+   `((,(abbreviate-file-name (eshell/pwd)) :foreground "#dcdccc")
+     (,(if (zerop (user-uid)) " # " " $ ") :foreground "green"))
+   ""))
+
+(setq eshell-highlight-prompt nil
+      eshell-prompt-function  #'my-eshell-prompt)
+
+(defun eshell-here ()
+  "Opens up a new shell in the directory associated with the
+current buffer's file. The eshell is renamed to match that
+directory to make multiple eshell windows easier."
+  (interactive)
+  (let* ((parent (if (buffer-file-name)
+                     (file-name-directory (buffer-file-name))
+                   default-directory))
+         (height (/ (window-total-height) 3))
+         (name   (car (last (split-string parent "/" t)))))
+    (split-window-vertically (- height))
+    (other-window 1)
+    (eshell "new")
+    (rename-buffer (concat "*eshell: " name "*"))
+
+    (insert (concat "ls"))
+    (eshell-send-input)))
+
+(global-set-key (kbd "C-!") 'eshell-here)
+
+(defun eshell/x ()
+  (insert "exit")
+  (eshell-send-input)
+  (delete-window))
+
 (use-package flyspell
   :ensure t
   :defer t
