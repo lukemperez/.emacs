5b445b44ffb422b1bfbe3a40531f4da013c08eb9
Luke M. Perez <lukemperez@gmail.com>
2024-10-05 14:28:09 -0700

    final 2019 MBP

--- .cache/customize.el
+++ .cache/customize.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(zenburn-theme solarized-theme quarto-mode pandoc-mode eshell-syntax-highlighting auctex)))
+   '(zenburn-theme solarized-theme quarto-mode pandoc-mode eshell-syntax-highlighting edit-indirect auctex)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- config.el
+++ config.el
@@ -121,25 +121,6 @@ Should end with a forward slash.")
 (setq LaTeX-biblatex-use-Biber t)
 (setq TeX-command-BibTeX "Biber")
 
-;; remove slashes when presenting italice
-; (setq org-hide-emphasis-markers t)
-
-;; (let* ((variable-tuple (cond ((x-list-fonts "Source Sans Pro") '(:font "Source Sans Pro"))
-;;                              (nil (warn "Cannot find a Sans Serif Font.  Install Source Sans Pro."))))
-;;        (base-font-color     (face-foreground 'default nil 'default))
-;;        (headline           `(:inherit default :weight bold :foreground ,base-font-color)))
-
-;;   (custom-theme-set-faces 'user
-;;                           `(org-level-8 ((t (,@headline ,@variable-tuple))))
-;;                           `(org-level-7 ((t (,@headline ,@variable-tuple))))
-;;                           `(org-level-6 ((t (,@headline ,@variable-tuple))))
-;;                           `(org-level-5 ((t (,@headline ,@variable-tuple))))
-;;                           `(org-level-4 ((t (,@headline ,@variable-tuple :height 1.1))))
-;;                           `(org-level-3 ((t (,@headline ,@variable-tuple :height 1.15))))
-;;                           `(org-level-2 ((t (,@headline ,@variable-tuple :height 1.25))))
-;;                           `(org-level-1 ((t (,@headline ,@variable-tuple :height 1.35))))
-;;                           `(org-document-title ((t (,@headline ,@variable-tuple :height 1.5 :underline nil))))))
-
 (require 'ox-md)
 (require 'ox-beamer)
 (require 'ox-latex)
@@ -171,9 +152,6 @@ Should end with a forward slash.")
 ;; (require 'org-ref)
 ;; (require 'bibtex)
 
-;; (require 'yaml-mode)
-  ;; (add-to-list 'auto-mode-alist '("\\.yml\\'" . yaml-mode))
-
 (use-package markdown-mode
   :ensure t
   :commands (markdown-mode gfm-mode)
@@ -200,7 +178,7 @@ Should end with a forward slash.")
   :mode
   ("\\.Snw" . poly-noweb+r-mode)
   ("\\.Rnw" . poly-noweb+r-mode)
-  ("\\.Rmd" . poly-markdown+r+mode)
+  ("\\.Rmd" . poly-+r+mode)
   ("\\.md" . poly-markdown-mode)
   )
 
@@ -340,3 +318,25 @@ Should end with a forward slash.")
 
 ;; Handy key definition
 (define-key global-map "\M-Q" 'unfill-paragraph)
+
+;; (require 'yaml-mode)
+  ;; (add-to-list 'auto-mode-alist '("\\.yml\\'" . yaml-mode))
+
+;; remove slashes when presenting italice
+; (setq org-hide-emphasis-markers t)
+
+;; (let* ((variable-tuple (cond ((x-list-fonts "Source Sans Pro") '(:font "Source Sans Pro"))
+;;                              (nil (warn "Cannot find a Sans Serif Font.  Install Source Sans Pro."))))
+;;        (base-font-color     (face-foreground 'default nil 'default))
+;;        (headline           `(:inherit default :weight bold :foreground ,base-font-color)))
+
+;;   (custom-theme-set-faces 'user
+;;                           `(org-level-8 ((t (,@headline ,@variable-tuple))))
+;;                           `(org-level-7 ((t (,@headline ,@variable-tuple))))
+;;                           `(org-level-6 ((t (,@headline ,@variable-tuple))))
+;;                           `(org-level-5 ((t (,@headline ,@variable-tuple))))
+;;                           `(org-level-4 ((t (,@headline ,@variable-tuple :height 1.1))))
+;;                           `(org-level-3 ((t (,@headline ,@variable-tuple :height 1.15))))
+;;                           `(org-level-2 ((t (,@headline ,@variable-tuple :height 1.25))))
+;;                           `(org-level-1 ((t (,@headline ,@variable-tuple :height 1.35))))
+;;                           `(org-document-title ((t (,@headline ,@variable-tuple :height 1.5 :underline nil))))))
